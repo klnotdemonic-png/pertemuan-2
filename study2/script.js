d73d99ba8c906374881591f0b3623dd115fa52fb
klnotdemonic <klnotdemonic@gmail.com>
2025-11-01 09:05:54 +0700

    menambahkan study2

---
+++ study2/script.js
@@ -0,0 +1,10 @@
+let namaProduk1 = "Laptop";
+let namaProduk2 = "Mouse";
+
+let hargaProduk1 = 7500000;
+let hargaProduk2 = 250000;
+
+function hitungTotal() {
+  let total = hargaProduk1 + hargaProduk2;
+  alert(`Total harga untuk produk ${namaProduk1} dan ${namaProduk2} adalah Rp${total}`);
+}
