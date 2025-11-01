d1ecdd46286031314828127c6c98dd326bf19037
klnotdemonic <klnotdemonic@gmail.com>
2025-11-01 08:20:32 +0700

    aritmatika

---
+++ aritmatika.js
@@ -0,0 +1,24 @@
+let a = 10;
+let b = 5;
+
+// Operator aritmatika
+let hasilTambah = a + b;   // 15
+let hasilKurang = a - b;   // 5
+let hasilKali = a * b;     // 50
+let hasilBagi = a / b;     // 2
+let hasilModulus = a % b;  // 0
+
+// Operator perbandingan
+let lebihBesar = a > b;    // true
+let lebihKecil = a < b;    // false
+let samaDengan = a === b;  // false
+let tidakSama = a !== b;   // true
+
+console.log("Hasil Tambah:", hasilTambah);
+console.log("Hasil Kurang:", hasilKurang);
+console.log("Hasil Kali:", hasilKali);
+console.log("Hasil Bagi:", hasilBagi);
+console.log("Hasil Modulus:", hasilModulus);
+
+console.log("Apakah a lebih besar dari b?", lebihBesar);
+console.log("Apakah a sama dengan b?", samaDengan);
