3cea28303680cbc7c79b8e1a2ef1e2d080aa2e20
klnotdemonic <klnotdemonic@gmail.com>
2025-11-01 08:17:36 +0700

    3 variabel

---
+++ 3variab.js
@@ -0,0 +1,7 @@
+let nama = "Fharel";     // Tipe data string
+const umur = 20;         // Tipe data number (konstanta, nilainya tidak bisa diubah)
+var aktif = true;        // Tipe data boolean
+
+console.log(nama); // Output: Fharel
+console.log(umur); // Output: 20
+console.log(aktif); // Output: true
